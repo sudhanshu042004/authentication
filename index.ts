0e395f88acfde1ed8ba1f8e53e7dbc0c93043d25
sudhanshu042004 <sudhanshusaudiyal15@gmail.com>
2025-12-19 23:31:25 +0530

    improve the cors error

--- index.ts
+++ index.ts
@@ -10,11 +10,12 @@ const port = parseInt(process.env.PORT!) || 3000;
 const app = express();
 
 app.use(cors({ origin: true, credentials: true }));
-app.options('*', cors({ origin: true, credentials: true }));
+// app.options('*', cors({ origin: true, credentials: true }));
 app.use(express.json());
 
 app.use(limitMiddleWare)
-app.get('/',(req : Request,res:Response)=>{
+
+app.get('/',(_req : Request,res:Response)=>{
     res.status(200).json({"message": "you have pinged the server"});
 })
 
